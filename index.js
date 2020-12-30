2ff0acdcf820a6e87360754d1ffe1aa516064e34
Akbar Lintang Aji <lintangajiakbar@gmail.com>
2020-12-30 14:58:15 +0700

    Finishing UI and fixing responsiveness

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('transparent');
@@ -5,3 +6,16 @@ $(window).scroll(function() {
         $('nav').removeClass('transparent');
     }
 });
+
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 300) {
+        $('.about-us-msg').fadeIn();
+    } else {
+        $('.about-us-msg').fadeOut();
+    }
+});
+
+*/
+
+AOS.init();
